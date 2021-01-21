2056173f6092c0dc5586ffe10758e927ec330fa0
Makarovskaia <iuliia.makarovskaia@t-systems.ru>
2021-01-21 11:06:38 +0300

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countFloors = scanner.nextInt();
 
+        if (countFloors > 0 && countFloors < 5) {
+            System.out.println("Малоэтажный дом");
+        } else if (countFloors >= 5 && countFloors < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else if (countFloors >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
