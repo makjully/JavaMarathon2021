bd68dfd8f17a419f3b77f928f5ca077adbef744d
iumakaro <iuliia.makarovskaia@t-systems.com>
2021-01-22 12:44:04 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double dividend;
+        double divider;
 
+        while (true) {
+            dividend = scanner.nextDouble();
+            divider = scanner.nextDouble();
+            if (divider == 0) {
+                break;
+            } else {
+                System.out.println(dividend / divider);
+            }
+        }
     }
 }
