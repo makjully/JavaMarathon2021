507ada31e3c976f7dc3555ceb5636981fedfbbd3
Makarovskaia <iuliia.makarovskaia@t-systems.ru>
2021-01-24 11:11:37 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,42 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("Midnight green");
+        car.setYearOfManufacture(2016);
+        car.setModel("Tesla Model S");
+        System.out.println("Автомобиль " + car.getModel() +
+                " в цвете " + car.getColor() + " " +
+                car.getYearOfManufacture() + " года выпуска");
+    }
+}
 
+class Car {
+    private int yearOfManufacture;
+    private String model;
+    private String color;
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,44 @@
 package day5;
 
-
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike("Harley-Davidson Road King", "Billiard Red", 2020);
+        System.out.printf("Мотоцикл %s в цвете %s %d года выпуска", motorbike.getModel(), motorbike.getColor(), motorbike.getYearOfManufacture());
+    }
+}
 
+class Motorbike {
+    private int yearOfManufacture;
+    private String model;
+    private String color;
+
+    Motorbike(String model, String color, int yearOfManufacture) {
+        this.model = model;
+        this.color = color;
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
