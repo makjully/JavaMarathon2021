dc9792e84b1961274009d0b81d97786248de021a
iumakaro <iuliia.makarovskaia@t-systems.com>
2021-01-27 12:53:00 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        String string = "";
 
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i + " ");
+        }
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(sb);
+        System.out.println("Время исполнения (StringBuilder): " + timeConsumedMillis); // = 20
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            string += i + " ";
+        }
+        finish = System.currentTimeMillis();
+        timeConsumedMillis = finish - start;
+        System.out.println(string);
+        System.out.println("Время исполнения (String): " + timeConsumedMillis); // = 170
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Airbus A320", 1994, 38, 77000);
 
+        System.out.println(airplane);
     }
 }
