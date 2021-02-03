f9d92f393030551ad9fefa88fdf7bf062730231c
iumakaro <iuliia.makarovskaia@t-systems.com>
2021-02-03 11:37:41 +0300

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,28 @@
 package day15;
 
+import java.io.*;
+
 public class Task1 {
     public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("src/main/resources/shoes.csv"));
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("src/main/resources/missing_shoes.txt"));
 
+            while (reader.ready()) {
+                String[] elements = reader.readLine().split(";");
+                if (Integer.parseInt(elements[2]) == 0) {
+                    writer.write(elements[0] + ", " + elements[1] + ", " + elements[2]);
+                    writer.newLine();
+                }
+            }
+
+            reader.close();
+            writer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
