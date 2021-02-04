5606fc0da83c55e2c7ad199af29a1b4b0240a0bc
Makarovskaia <iuliia.makarovskaia@t-systems.ru>
2021-02-04 13:38:43 +0300

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,37 @@
 package day16;
 
+import java.io.*;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("0202.txt");
+        printResult(file);
+    }
 
+    public static void printResult(File file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            int count = 0;
+            int sum = 0;
+
+            while (reader.ready()) {
+                String[] line = reader.readLine().split(" ");
+                for (int i = 0; i < line.length; i++) {
+                    sum += Integer.parseInt(line[i]);
+                    count++;
+                }
+            }
+            reader.close();
+
+            if (count > 0) {
+                double arithmeticalMean = (double) sum / count;
+                System.out.print(arithmeticalMean);
+                System.out.printf(" --> %.3f", arithmeticalMean);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,47 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Random;
 
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        File file1 = new File("C:\\Users\\iumakaro\\Documents\\JavaMarathon\\JavaMarathon2021\\file1.txt");
+        File file2 = new File("C:\\Users\\iumakaro\\Documents\\JavaMarathon\\JavaMarathon2021\\file2.txt");
+        BufferedWriter writer1 = new BufferedWriter(new FileWriter(file1));
+        BufferedWriter writer2 = new BufferedWriter(new FileWriter(file2));
+
+        for (int i = 0; i < 1000; i += 20) {
+            int sum = 0;
+            for (int j = 0; j < 20; j++) {
+                Random random = new Random();
+                int number = random.nextInt(100);
+                sum += number;
+                writer1.write(number + " ");
+            }
+            double arithmeticalMean = (double) sum / 20;
+            writer2.write(arithmeticalMean + " ");
+        }
+        writer1.close();
+        writer2.close();
+
+        printResult(file2);
+    }
+
+    public static void printResult(File file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            double sumDouble = 0.0;
+
+            while (reader.ready()) {
+                String[] elements = reader.readLine().split(" ");
+                for (String element : elements) {
+                    sumDouble += Double.parseDouble(element);
+                }
+            }
+
+            System.out.println((int) sumDouble);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
