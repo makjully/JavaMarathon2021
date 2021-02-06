32a5832e1760a979ff2c488d1bcc0ad5e4dc0b0e
Makarovskaia <iuliia.makarovskaia@t-systems.ru>
2021-02-06 13:08:45 +0300

    complete day17 tasks

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,17 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] chessPieces = new ChessPiece[8];
 
+        for (int i = 0; i < chessPieces.length; i++) {
+            if (i < chessPieces.length / 2) {
+                chessPieces[i] = ChessPiece.PAWN_WHITE;
+            } else
+                chessPieces[i] = ChessPiece.ROOK_BLACK;
+        }
+
+        for (ChessPiece chessPiece : chessPieces) {
+            System.out.print(chessPiece + " ");
+        }
     }
 }
