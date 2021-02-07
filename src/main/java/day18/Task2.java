d47202536c24ffa364e61e073ed33eace06ddccf
Makarovskaia <iuliia.makarovskaia@t-systems.ru>
2021-02-07 23:39:50 +0300

    complete day18 tasks

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,29 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(count7(717771237));
+    }
 
+    public static int count7(int wholeNumber) {
+        if (wholeNumber < 0) {
+            return 0;
+        }
+
+        String s = String.valueOf(wholeNumber);
+
+        if (s.length() == 1) {
+            if (s.equals("7")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        int remaining = Integer.parseInt(s.substring(0, s.length() - 1));
+
+        if (s.substring(s.length() - 1).equals("7")) {
+            return 1 + count7(remaining);
+        }
+        return count7(remaining);
     }
 }
--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -3,5 +3,57 @@ package day18;
 public class Task3 {
     public static void main(String[] args) {
 
+        Node root = new Node(20);
+        addBranch(14, root);
+        addBranch(23, root);
+        addBranch(11, root);
+        addBranch(5, root);
+        addBranch(8, root);
+        addBranch(16, root);
+        addBranch(18, root);
+        addBranch(15, root);
+        addBranch(22, root);
+        addBranch(27, root);
+        addBranch(24, root);
+        addBranch(150, root);
+
+        dfs(root); // 5 8 11 14 15 16 18 20 22 23 24 27 150
+
+    }
+
+    public static void dfs(Node node) {
+        if (node.getLeft() != null) {
+            if (node.getValue() > node.getLeft().getValue()) {
+                dfs(node.getLeft());
+            }
+        }
+
+        System.out.print(node + " ");
+
+        if (node.getRight() != null) {
+            if (node.getValue() < node.getRight().getValue()) {
+                dfs(node.getRight());
+            }
+        }
+    }
+
+    public static void addBranch(int value, Node parent) {
+        while (true) {
+            if (value < parent.getValue()) {
+                if (parent.getLeft() != null) {
+                    parent = parent.getLeft();
+                } else {
+                    parent.setLeft(new Node(value));
+                    break;
+                }
+            } else if (value > parent.getValue()) {
+                if (parent.getRight() != null) {
+                    parent = parent.getRight();
+                } else {
+                    parent.setRight(new Node(value));
+                    break;
+                }
+            }
+        }
     }
 }
